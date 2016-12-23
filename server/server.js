25313d20e42b05f4c1049ee760d3bab856daf146
Chuducty <thuandu@gmail.com>
2016-12-23 18:38:10 +0700

    Add POST todo and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 3000
+
+
+var {mongoose} = require('./db/mongoose.js');
+var {User} = require('./models/user.js')
+var {Todo} = require('./models/todo.js')
+
+var app = express();
+
+app.use(bodyParser.json());
+
+
+
+app.post('/todos', (req,res) => {
+  var todo = new Todo ({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+});
+
+app.listen(port,() => {
+  console.log(`Server is ready on port ${port}`);
+});
